bc441f511b14eb267cfe942ff2d1f78c22cca614
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-09-26 22:24:54 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS-1-Assignment/m3/3-sum/Solution.java
+++ ADS-1-Assignment/m3/3-sum/Solution.java
@@ -4,43 +4,43 @@ import java.util.Arrays;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * { main function }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int array = scan.nextInt();
-		int[] threesum = new int[array];
-		for (int i = 0; i < threesum.length; i++) {
-			threesum[i] = scan.nextInt();
-		}
-		Arrays.sort(threesum);
-		int count = 0;
-		for (int i = 0; i < array - 2; i++) {
-			int j = i + 1;
-			int k = array - 1;
-			while (j < k) {
-				if (threesum[i] + threesum[j]
-					 + threesum[k] == 0) {
-					count++;
-					j++;
-					k--;
-				} else if (threesum[i] + threesum[j]
-					 + threesum[k] < 0) {
-					j++;
-				} else {
-					k--;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+    }
+    /**.
+     * { main function }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int array = scan.nextInt();
+        int[] threesum = new int[array];
+        for (int i = 0; i < threesum.length; i++) {
+            threesum[i] = scan.nextInt();
+        }
+        Arrays.sort(threesum);
+        int count = 0;
+        for (int i = 0; i < array - 2; i++) {
+            int j = i + 1;
+            int k = array - 1;
+            while (j < k) {
+                if (threesum[i] + threesum[j]
+                     + threesum[k] == 0) {
+                    count++;
+                    j++;
+                    k--;
+                } else if (threesum[i] + threesum[j]
+                     + threesum[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
