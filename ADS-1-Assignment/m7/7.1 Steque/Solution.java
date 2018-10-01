08248cbb0ff9ef3a7427b35778154ad94b5cc72a
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-10-01 20:27:21 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS-1-Assignment/m7/7.1 Steque/Solution.java
+++ ADS-1-Assignment/m7/7.1 Steque/Solution.java
@@ -1,49 +1,49 @@
 import java.util.Scanner;
-/**
+/**.
  *  client class.
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused constructor.
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Steque obj = new Steque();
-		int values = Integer.parseInt(scan.nextLine());
-		while (scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			if (line.length() == 0) {
-				obj.setSize(0);
-				System.out.println();
-			} else {
-				switch (tokens[0]) {
-				case "push":
-					obj.push(Integer.parseInt(
-					             tokens[1]));
-					obj.print();
-					break;
-				case "pop":
-					obj.pop();
-					obj.print();
-					break;
-				case "enqueue":
-					obj.enqueue(Integer.parseInt(tokens[1]));
-					obj.print();
-					break;
-				default:
-					break;
-				}
-			}
-		}
-	}
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Steque obj = new Steque();
+        int values = Integer.parseInt(scan.nextLine());
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            if (line.length() == 0) {
+                obj.setSize(0);
+                System.out.println();
+            } else {
+                switch (tokens[0]) {
+                case "push":
+                    obj.push(Integer.parseInt(
+                                 tokens[1]));
+                    obj.print();
+                    break;
+                case "pop":
+                    obj.pop();
+                    obj.print();
+                    break;
+                case "enqueue":
+                    obj.enqueue(Integer.parseInt(tokens[1]));
+                    obj.print();
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
 }
 
