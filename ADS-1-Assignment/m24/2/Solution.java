54f73892fd059e7dc0e480872f6398edabba2ab8
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-10-27 13:14:05 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-Assignment/m24/2/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int inputs = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < inputs; i++) {
+			String[] tokens = scan.nextLine().split(",");
+		}
+		int values = Integer.parseInt(scan.nextLine());
+		for (int j = 0; j < values; j++) {
+			String[] tokens1 = scan.nextLine().split(" ");
+		}
+	}
+}
